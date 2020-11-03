5288560548052edb35d845287047501e8bdf37f4
Thaís Apª. Vilarinho de Jesus <47863628+thaisvilarinho@users.noreply.github.com>
2020-11-02 23:30:06 -0300

    exercício 01

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("teste");
+const delay = () => new Promise(resolve =>
+    setTimeout(resolve(1), 1000));
+
+async function umPorSegundo() {
+    console.log(`${await delay()*1}s`);
+    console.log(`${await delay()*2}s`);
+    console.log(`${await delay()*3}s`);
+
+}
+
+umPorSegundo(); 
